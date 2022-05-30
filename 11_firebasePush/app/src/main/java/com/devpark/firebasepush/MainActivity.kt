97a2280997f90fa4a819c583855620cb5b0620b3
86gram <86gramsoft@gmail.com>
2022-05-30 18:18:35 +0900

    22..05.30 - Firebase Push Test 마무리

--- 11_firebasePush/app/src/main/java/com/devpark/firebasepush/MainActivity.kt
+++ 11_firebasePush/app/src/main/java/com/devpark/firebasepush/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.devpark.firebasepush
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -21,6 +23,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initFirebase()
+        updateResult()
+    }
+
+    /**
+     * Intent addFlag를 FLAG_ACTIVITY_SINGLE_TOP으로 주었을 때
+     * B Activity를 실행 시키면 onCreate를 실행시키지 않고
+     * onNewIntent 메서드를 실행 시킨
+     */
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d("onNewIntent", "onNewIntent Enter!!")
+
+        /**
+         * onCreate()에서 가져온 intent가 있기 때문에
+         * setIntent로 통해서 새로 들어온 intent로 교체를 해야 한다.
+         */
+        setIntent(intent)
+
+        updateResult(true)
     }
 
 
@@ -31,6 +52,18 @@ class MainActivity : AppCompatActivity() {
                 Log.d("FirebaseToken", task.result)
             }
         }
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        Log.d("updateResult", "updateResult Enter!!")
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") + if (isNewIntent) {
+            "(으)로 갱신했습니다."
+        } else {
+            "(으)로 실행했습니다."
+        }
+
     }
 
 }
--- 11_firebasePush/app/src/main/java/com/devpark/firebasepush/MyFirebaseMessagingService.kt
+++ 11_firebasePush/app/src/main/java/com/devpark/firebasepush/MyFirebaseMessagingService.kt
@@ -1,7 +1,21 @@
 package com.devpark.firebasepush
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.NotificationParams
 import com.google.firebase.messaging.RemoteMessage
+import java.nio.channels.Channel
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -22,9 +36,85 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     /**
      * Firebase에서 넘어오는 메시지 처리
      */
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        createNotificationChannel()
+
+        val type = remoteMessage.data["type"]?.let { NotificationType.valueOf(it) }
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+        type ?: return
+
+
+        NotificationManagerCompat.from(this).notify(type.id, createNotification(type, title, message))
 
     }
 
+    private fun createNotification(type: NotificationType, title: String?, message: String?): Notification {
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("notificationType", "${type.title} 타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getActivity(this, type.id, intent, PendingIntent.FLAG_MUTABLE)
+        } else {
+            PendingIntent.getActivity(this, type.id, intent, FLAG_UPDATE_CURRENT)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNER_ID)
+            .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        when (type) {
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+                notificationBuilder.setStyle(
+                    NotificationCompat.BigTextStyle().bigText(
+                        "\uD83E\uDD17\uD83D\uDE02\uD83D\uDE02\uD83C\uDF77\uD83D\uDE4F\uD83D\uDE09\uD83D\uDC4D✅\uD83E\uDD14\uD83D\uDE43\uD83D\uDCE2\uD83D\uDE42\uD83D\uDE04✔️\uD83D\uDC95÷☀️🤗😂😂🍷🙏😉👍✅🤔🙃📢🙂😄✔️💕÷☀️\uD83E\uDD17\uD83D\uDE02\uD83D\uDE02\uD83C\uDF77\uD83D\uDE4F\uD83D\uDE09\uD83D\uDC4D✅\uD83E\uDD14\uD83D\uDE43\uD83D\uDCE2\uD83D\uDE42\uD83D\uDE04✔️\uD83D\uDC95÷☀️"
+                    )
+                )
+            }
+            NotificationType.CUSTOM -> {
+                notificationBuilder
+                    .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                    .setCustomContentView(
+                        RemoteViews(
+                            packageName,
+                            R.layout.view_custom_notification
+                        ).apply {
+                            setTextViewText(R.id.title, title)
+                            setTextViewText(R.id.message, message)
+                        }
+                    )
+            }
+        }
+
+        return notificationBuilder.build()
+
+    }
+
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNER_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(channel)
+        }
+    }//end of createNotificationChannel
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNER_ID = "Channel Id"
+    }
 }

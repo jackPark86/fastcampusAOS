87e7e6ed25bfe5eadfa638571de60c9ee70fec96
icoinnet <jackpark086@gmail.com>
2022-10-05 13:17:11 +0900

    22.10.5 - mypage 완료

---
+++ 16_carrotMarket/app/src/main/java/com/example/carrotmarket/AddArticleActivity.kt
@@ -0,0 +1,189 @@
+package com.example.carrotmarket
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.icu.text.CaseMap
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import com.example.carrotmarket.DBKey.Companion.DB_ARTICLES
+import com.example.carrotmarket.home.ArticleModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+
+class AddArticleActivity : AppCompatActivity() {
+    companion object {
+        private val REQUEST_CODE = 1010
+    }
+
+    private var selectedUri: Uri? = null
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+    private val storage: FirebaseStorage by lazy {
+        Firebase.storage
+    }
+    private val articleDB: DatabaseReference by lazy {
+        Firebase.database.reference.child(DB_ARTICLES)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_article)
+
+        findViewById<Button>(R.id.imageAddButton).setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    startContentProvider()
+                }
+                /**
+                 * shouldShowRequestPermissionRationale
+                 * 사용자가 권한 요청을 명시적으로 거부한 경우 true를 반환한다.
+                 * 사용자가 권한 요청을 처음 보거나, 다시 묻지 않음 선택한 경우, 권한을 허용한 경우 false를 반환한다.
+                 */
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    showPermissionContextPopup()
+                }
+
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        REQUEST_CODE
+                    )
+                }
+            }
+        }
+
+        findViewById<Button>(R.id.submitButton).setOnClickListener {
+            val title = findViewById<EditText>(R.id.titleEditText).text.toString()
+            val price = findViewById<EditText>(R.id.priceEditText).text.toString()
+            val sellerId = auth.currentUser?.uid.orEmpty()
+
+            showProgress()
+
+            if (selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                uploadPhoto(
+                    photoUri,
+                    successHandler = { uri ->
+                        uploadArticle(sellerId, title, price, uri)
+
+                    },
+                    errorHandler = {
+                        Toast.makeText(this, "사진 업로드에 실패했습니다", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            } else {
+                uploadArticle(sellerId, title, price, "")
+            }
+
+
+        }
+
+    }
+
+    private fun uploadPhoto(uri: Uri, successHandler: (String) -> Unit, errorHandler: () -> Unit) {
+        val fileName = "${System.currentTimeMillis()}.png"
+        storage.reference.child("article/photo").child(fileName)
+            .putFile(uri)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    storage.reference.child("article/photo").child(fileName)
+                        .downloadUrl
+                        .addOnSuccessListener { uri ->
+                            successHandler(uri.toString())
+                        }.addOnFailureListener {
+                            errorHandler()
+                            hideProgress()
+                        }
+                } else {
+                    errorHandler()
+                    hideProgress()
+                }
+            }
+
+    }
+
+    private fun uploadArticle(sellerId: String, title: String, price: String, imageUrl: String) {
+        val model = ArticleModel(sellerId, title, System.currentTimeMillis(), "$price 원", imageUrl)
+        articleDB.push().setValue(model)
+        hideProgress()
+        finish()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE ->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startContentProvider()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+    }
+
+    private fun startContentProvider() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startForResult.launch(intent)
+    }
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val uri = result.data?.data
+                if (uri != null) {
+                    findViewById<ImageView>(R.id.photoImageView).setImageURI(uri)
+                    selectedUri = uri
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못해습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    private fun showProgress() {
+        findViewById<ProgressBar>(R.id.progressBar).isVisible = true
+    }
+
+    private fun hideProgress() {
+        findViewById<ProgressBar>(R.id.progressBar).isVisible = false
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("사진을 가져오기 위해 필요합니다.")
+            .setPositiveButton("동의") { _, _ ->
+                requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_CODE
+                )
+            }
+            .create()
+            .show()
+    }//end of showPermissionContextPopup
+
+}
---
+++ 16_carrotMarket/app/src/main/java/com/example/carrotmarket/DBKey.kt
@@ -0,0 +1,8 @@
+package com.example.carrotmarket
+
+class DBKey {
+
+    companion object {
+        const val DB_ARTICLES = "Articles"
+    }
+}
